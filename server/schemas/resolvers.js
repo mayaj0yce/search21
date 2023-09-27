7c96d2b5a2bba50b1c13d6b547ccbc5cbd3373db
mayaj0yce <m.j.thayne@gmail.com>
2023-09-27 13:13:01 -0600

    resolvers should be done

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -29,12 +29,43 @@ addUser: async (parent, args) => {
     //if NO user found then throw the authentication error 
     //this allows the user to know WHY the error is occurring 
  
-    const correctPassw = await user.isCorrectPassword(password);
-    
+    const CorrectPassword = await user.isCorrectPassword(password);
+
+    //checks to see if the password is valid and for the assigned user
+
+    if (!CorrectPassword) {
+        throw new AuthenticationError('wrong password');
+    }
+
+    const token = signToken(user);
+    //assigns the token to user which is specific for schema form 
+    return { token, user };
+  },
+  saveBook: async (parent, { newBook }, context) => {
+    if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+            { _id: context.user._id },
+            { $push: { savedBooks: newBook }},
+            { new: true}
+        );
+        return updatedUser;
+        //allows user to save books to their own profile
+    }
+    throw new AuthenticationError('log in to use this feature');
+    //BUT they MUST be logged in and verified to save 
+  },
+  removeBook: async (parent, { bookId }, context) => {
+    if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+            { _id: context.user._id },
+            { $push: { savedBooks: { bookId }}},
+            { new: true}
+        );
+        return updatedUser;
   }
-}
+throw new AuthenticationError('log in to use this feature');
+},
+},
+};
 
-
-
-
-}}
+module.exports = resolvers;
